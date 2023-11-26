fa5a35dc0f10bd59f3fcae360626aebf0dd4a48b
Rajesh Kannan M <rajeshkannan.offl@gmail.com>
2023-11-26 22:57:33 +0530

    Git Bash Created git bash files for auto-push. Use git_add.sh to add all the new/changed files. Do git commit and use git_push.sh to push files with message. Use git.sh to push files with default message.

---
+++ git_add.sh
@@ -0,0 +1,9 @@
+sleep 1
+git init
+sleep 2
+git status
+sleep 2
+git add .
+sleep 2
+git status
+sleep 1
---
+++ git_push.sh
@@ -0,0 +1,7 @@
+sleep 1
+git push -f
+sleep 2
+echo ""
+echo -n "Successfully Committed to Git!"
+echo ""
+sleep 1
